eea3c3a088711b4de2448e405a1841e284f7a3fe
Artur <melaniasongsx@gmail.com>
2025-06-09 14:20:02 -0300

    v1

--- meufront/src/pages/Login.jsx
+++ meufront/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./login.css";
+import "../pages/Login.css";
 import { FaEnvelope, FaLock } from "react-icons/fa";
 
 function Login() {
